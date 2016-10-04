923fa4ff81acef9131a64e1c126fa5eea217d269
juusoko <juuso.1.korhonen@gmail.com>
2016-10-04 13:41:58 +0300

    lisää

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+var Graph = require('node-dijkstra');
+var ReactDOM = require('react-dom');
+var React = require('react');
+//var mongodb = require('mongodb');
+//var MongoClient = mongodb.MongoClient;
+var url = "mongodb://localhost:27017/etaisyydet";
+/*MongoClient.connect(url, function(err, db){
+	if (err){
+		console.log(err);
+	} else {
+		console.log("Mongo done");
+		// db actions
+		db.close();
+	}
+});
+*/
+$(document).ready(function(){
+	console.log("qweq");
+	$("#buttondiv").text(getData("tampere"));
+	test();
+});
+function test(){
+	var route = new Graph();
+	route.addNode('A', { B: 1, C: 2});
+	route.addNode('B', { A: 1});
+	route.addNode('C', { A: 2});
+	console.log(route.path('B', 'C', { cost: true }));
+}
+function getData(haettavaKaupunki){
+	console.log("getdata");
+	$.ajax({
+			url: "/api/"+haettavaKaupunki,
+			success: function(response){
+				console.log(response);
+				return response;
+			}
+	});
+}
+console.log("asd");
 ReactDOM.render(
-	<h1>Hello, world!</h1>,
-     	document.getElementById('example')
+	<div>Hello, world!</div>,
+     	document.getElementById('hellodiv')
 );
-
--- webapp.js
+++ webapp.js
@@ -1,10 +1,29 @@
-var express = require('express')
+var express = require('express');
+
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 
 app.use(express.static('static'));
 
-var server = app.listen(3000, function() {
-	var port = server.address().port;
-	console.log("Started server at port", port);
+var routes = require("./static/api.js")(app);
+
+var bugData = [
+	{id: 1, priority: 'P1', status:'Open', owner:'Ravan', title:'App crashes on open'},
+	{id: 2, priority: 'P2', status:'New', owner:'Eddie', title:'Misaligned border on panel'}
+];
+
+app.get('/api/tampere', function(req, res) {
+	db.collection('etaisyydet').find({"Nimi":"Tamperes"}).toArray(function(err, docs){
+		res.json(docs);
+	});
+});
+
+
+MongoClient.connect('mongodb://localhost/etaisyydet', function(err, dbConnection) {
+  db = dbConnection;
+  var server = app.listen(3000, function() {
+	  var port = server.address().port;
+	  console.log("Started server at port", port);
+  });
 });
